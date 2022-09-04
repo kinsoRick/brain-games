974f4ecce36a4476ae5a706e5b9c55a1f23edcf0
kinsoRick <kmasnaveev@gmail.com>
2022-09-04 10:30:58 +0300

    fix(lint): warnings fixed

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,11 @@
 import RLS from 'readline-sync';
 
-
-export const greeting = () => {
+const greeting = () => {
   console.log('Welcome to the Brain Games!');
 
   const name = RLS.question('May I have your name? ');
 
   return console.log(`Hello, ${name}!`);
-}
+};
+
+export default greeting;
