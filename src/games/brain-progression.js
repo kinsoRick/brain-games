c3004c4000e6c93b61a88708e04ad68cb970d617
kinsoRick <kmasnaveev@gmail.com>
2022-09-04 17:59:30 +0300

    docs(comment)

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -2,15 +2,16 @@ import playGame from '../index.js';
 import getRandomNumber from '../utils.js';
 
 const progressionGameQA = () => {
-  const d = getRandomNumber(10) + 1;
+  const step = getRandomNumber(10) + 1;
   const missingPosition = getRandomNumber(10);
   const progression = [];
 
+  // startNumber
   let number = getRandomNumber(20);
 
   for (let i = 0; i < 10; i += 1) {
     progression.push(number);
-    number += d;
+    number += step;
   }
 
   const solution = progression[missingPosition];
