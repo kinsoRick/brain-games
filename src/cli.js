59b1f4aab97f9b330aa5f832f197811ddb18f07a
kinsoRick <kmasnaveev@gmail.com>
2022-09-04 16:00:02 +0300

    fix(lint): identical block fixed

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,3 @@
-import RLS from 'readline-sync';
-
-const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-
-  const name = RLS.question('May I have your name? ');
-
-  console.log(`Hello, ${name}!`);
-
-  return name;
-};
+import { greeting } from './index.js';
 
 export default greeting;
