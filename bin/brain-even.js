72914d4b33bb0de0bf4101c758c130ba1ea434e0
kinsoRick <kmasnaveev@gmail.com>
2022-09-04 15:54:27 +0300

    feat(games): all game logic stored in the index.js

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,31 +1,16 @@
 #!/usr/bin/env node
-import RLS from 'readline-sync';
-import greeting from '../src/cli.js';
+import playGame from '../src/index.js';
+import getRandomNumber from '../src/utils.js';
 
-const getRandomNumber = (max) => Math.floor(Math.random() * max);
+const evenGameQA = () => {
+  const number = getRandomNumber(100);
+  const isEvenNumber = (number % 2 === 0) ? 'yes' : 'no';
 
-const evenGame = () => {
-  // Get name from input in greeting function;
-  const name = greeting();
-  console.log('Answer "yes" if the number is even, otherwise answer "no"');
-
-  let correctAnswers = 0;
-  while (correctAnswers < 3) {
-    const number = getRandomNumber(100);
-    const isEvenNumber = (number % 2 === 0) ? 'yes' : 'no';
-
-    const answer = RLS.question(`Question: ${number}\nYour answer: `);
-
-    if (answer === isEvenNumber) {
-      correctAnswers += 1;
-      console.log('Correct!');
-    } else {
-      correctAnswers = 0;
-      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
-    }
-  }
-
-  console.log(`Congratulations, ${name}!`);
+  return { question: number, answer: isEvenNumber };
 };
 
-evenGame();
+playGame(
+  3,
+  'Answer "yes" if the number is even, otherwise answer "no"',
+  evenGameQA,
+);
---
+++ src/index.js
@@ -0,0 +1,37 @@
+import RLS from 'readline-sync';
+
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+
+  const name = RLS.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+
+  return name;
+};
+
+const playGame = (countOfRounds, gameRules, getQA) => {
+  const name = greeting();
+  console.log(gameRules);
+
+  let correctAnswers = 0;
+
+  while (correctAnswers < countOfRounds) {
+    const { question, answer } = getQA();
+
+    console.log(`Question: ${question}`);
+    const userAnswer = RLS.question('Your answer: ');
+
+    if (userAnswer === answer) {
+      correctAnswers += 1;
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${answer}'.\nLet's try again, ${name}!`);
+      break;
+    }
+  }
+
+  if (correctAnswers === countOfRounds) console.log(`Congratulations, ${name}!`);
+};
+
+export default playGame;
