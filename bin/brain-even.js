8aa78f2d951c7292e9ef21f214b09f5db9911d36
kinsoRick <kmasnaveev@gmail.com>
2022-09-04 13:10:17 +0300

    refactor

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,27 +5,25 @@ import greeting from '../src/cli.js';
 const getRandomNumber = (max) => Math.floor(Math.random() * max);
 
 const evenGame = () => {
+  // Get name from input in greeting function;
   const name = greeting();
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
 
   let correctAnswers = 0;
-
   while (correctAnswers < 3) {
     const number = getRandomNumber(100);
-    let isEvenNumber = 'no';
-
-    if (number % 2 === 0) {
-      isEvenNumber = 'yes';
-    }
+    const isEvenNumber = number % 2 === 0 ? 'yes' : 'no';
 
     const answer = RLS.question(`Question: ${number}\nYour answer: `);
 
     if (answer === isEvenNumber) {
       correctAnswers += 1;
       console.log('Correct!');
-    } else {
+    }
+
+    if (answer !== isEvenNumber) {
       correctAnswers = 0;
-      console.log('"yes" is wrong answer ;(. Correct answer was "no".\nLet\'s try again, Bill!');
+      console.log(`'yes' is wrong answer ;(. Correct answer was 'no'.\nLet's try again, ${name}!`);
     }
   }
 
